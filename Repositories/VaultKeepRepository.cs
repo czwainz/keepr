a284b3587661265d7dbc4d3ecf66725b5fdce046
Chrissy Zwainz <zchrissychrissy@gmail.com>
2019-01-20 18:56:15 -0700

    added files

---
+++ Repositories/VaultKeepRepository.cs
@@ -0,0 +1,23 @@
+
+using System;
+using System.Collections.Generic;
+using Dapper;
+using Keepr.Models;
+using System.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keepr.Repositories
+{
+
+  public class VaultKeepRepository
+  {
+    private readonly IDbConnection _db;
+
+    public VaultKeepRepository(IDbConnection db)
+    {
+      _db = db;
+    }
+  }
+
+}
